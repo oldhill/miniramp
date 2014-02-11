1d8856874ca56fdaed548e007cd7949309e903f5
oldhill <oldhillcode@gmail.com>
2014-02-10 22:35:57 -0800

    now getting artist data from SoundCloud API

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import urllib2
 
 import webapp2
 import jinja2
@@ -22,11 +23,18 @@ class MainHandler(webapp2.RequestHandler):
 
 class Recommender(webapp2.RequestHandler):
   def get(self, artistUsername):
+
+    # 'resolve' URL is necessary to get info based on artist's
+    # username instead of user ID number.
+    # Info: http://developers.soundcloud.com/docs/api/reference#resolve
+    resolveUrl = 'http://api.soundcloud.com/resolve.json?url='
+    knownUrl = 'http://soundcloud.com/' + artistUsername + '&client_id=YOUR_CLIENT_ID' 
+    fullUrl = resolveUrl + knownUrl
+
+    artistObject = urllib2.urlopen(fullUrl).read()
+
     self.response.headers['Content-Type'] = 'application/json'
-    artistObj = {
-      'test' : artistUsername,
-    }
-    self.response.out.write(json.dumps(artistObj))
+    self.response.out.write(artistObject)
 
 
 app = webapp2.WSGIApplication([
