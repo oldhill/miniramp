fc158da4a27c6a22ee0d43e8819d0ca2b1f9012d
oldhill <oldhillcode@gmail.com>
2014-04-20 14:37:40 -0700

    Small logging update and import order fix

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 Recommends SoundCloud artists based on a user input artist. 
 by oldhill.  MIT license.
 """
-
 import json
 import operator
-import urllib2
 import os
+import urllib2
 
 import jinja2
 import logging
@@ -64,10 +63,7 @@ class Recommender(webapp2.RequestHandler):
                             key = operator.itemgetter('occurrence_count'),
                             reverse = True)
     top_eight = sorted_artists[0:8]
-
-    # Debug
-    logging.info(top_eight)
-    logging.info('Total # of artists: ' + str(len(artist_list)))
+    logging.info('Top 8 artists: %s of a total # of %s artists' % (top_eight, len(artist_list))
 
     self.response.headers['Content-Type'] = 'application/json'
     self.response.out.write(json.dumps(top_eight))
